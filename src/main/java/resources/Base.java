70f32df0ff355a1535c6fc14432e36baea9682b8
Isha Shetty <isha.godyal29292@gmail.com>
2022-06-06 10:04:08 +0530

    Test base class to initail driver and getting browsername from properties file

---
+++ src/main/java/resources/Base.java
@@ -0,0 +1,56 @@
+package resources;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Base {
+
+	public static WebDriver driver;
+	
+	public WebDriver initializeDriver() throws IOException
+	{
+		//chrome Driver
+		
+		Properties prop = new Properties();   //create DataDriven Property file
+		FileInputStream fis = new FileInputStream("C:\\Users\\isha.godyal\\workspace\\E2EProject\\src\\main\\java\\Academy\\data.properties");   //give File Path where exactly the data driven file located
+		
+		prop.load(fis); //load will take fis as a argument and pass fis obj have knowledge of this property
+		String browserName=prop.getProperty("browser"); // goes to data properties file and pulls out the browser to base file
+		
+		if(browserName.equals("chrome"))
+		{
+			//execute in chrome driver
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\isha.godyal\\workspace\\E2EProject\\resources\\chromedriver.exe");
+			
+			//webdriver.chrome.driver->value of path
+			 driver = new ChromeDriver();
+		}
+		else if (browserName.equals("IE"))
+		{
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\isha.godyal\\workspace\\E2EProject\\resources\\IEDriverServer.exe");
+			
+			//webdriver.chrome.driver->value of path
+			 driver = new InternetExplorerDriver();
+		}
+		else if (browserName.equals("firefox"))
+		{
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\isha.godyal\\workspace\\E2EProject\\resources\\geckodriver.exe");
+			DesiredCapabilities capabilities = DesiredCapabilities.firefox();  
+	        capabilities.setCapability("marionette",true);  
+			//webdriver.chrome.driver->value of path
+			 driver = new FirefoxDriver(capabilities);
+		}
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+	}
+}
