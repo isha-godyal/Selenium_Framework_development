14daddd9bd913183fdf777435bc4ba6a1e8bb73c
Isha Shetty <isha.godyal29292@gmail.com>
2022-06-06 21:54:24 +0530

    Parameterization Test with Multiple Test Data : Data Provider Concept

--- src/test/java/Academy/HomePage.java
+++ src/test/java/Academy/HomePage.java
@@ -1,12 +1,13 @@
 package Academy;
 
+import org.testng.annotations.Test;
 import java.io.IOException;
 
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.DataProvider;
 
 import pageObjects.LandingPage;
 import pageObjects.LoginPage;
@@ -14,8 +15,8 @@ import resources.Base;
 
 public class HomePage extends Base {
 
-	@Test
-	public void basePageNavigation() throws IOException {
+	@Test(dataProvider="getData")
+	public void basePageNavigation(String Username, String Password, String text) throws IOException {
 		
 		driver = initializeDriver(); // invoke a method to initialize driver
 		
@@ -36,7 +37,26 @@ public class HomePage extends Base {
 		LoginPage lp = new LoginPage(driver);
 		lp.getEmail().sendKeys("abc@qw.com");
 		lp.getPassword().sendKeys("123456");
+		System.out.println(text);
 		lp.getLogin().click();
 	}
 
+	@DataProvider
+	public Object[][] getData()
+	{
+		//Rows stands for how many different data types test shud run
+		//column stands for how many values per each test
+		//0th row
+		Object[][] data = new Object[2][3];
+		data[0][0] = "nonrestricteduser@qw.com";
+		data[0][1] = "123456";
+		data[0][2] = "Restricted User";
+		
+		//1st row
+		data[1][0] = "restricteduser@qw.com";
+		data[1][1] = "456788";
+		data[1][2] = "Non restricted user";
+		
+		return data;
+	}
 }
